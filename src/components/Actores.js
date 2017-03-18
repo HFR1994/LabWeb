735c0b8d22c270d983d7f6ada68f83fc74906b34
Héctor Flores <frhectorin@gmail.com>
2017-03-18 11:57:00 -0600

    Added serie

--- src/components/Actores.js
+++ src/components/Actores.js
@@ -3,19 +3,19 @@ import '../css/Actores.css';
 import Actor from "./Actor";
 import Cruz from "../images/remove.png"
 
-const Actores = ({ name, actors=[], remove= f => f} ) =>
+const Actores = ({ id, name, actores=[], remove= f => f} ) =>
 
     <div className="card_holder">
-        <img alt="remove" className="remove_icon" onClick={() => remove(3)} src={Cruz}/>
+        <img alt="remove" className="remove_icon" onClick={() => remove(id)} src={Cruz}/>
         <div className="title_holder">
-            <h3 className="title">Game of Thrones</h3>
-            <div className="id_value">83</div>
+            <h3 className="title">{name}</h3>
+            <div className="id_value">{id}</div>
         </div>
         <div className="container_list">
             <ul className="actores">
-                {(actors.length === 0) ? <p id="empty">Agrega un Actor</p>
+                {(actores.length === 0) ? <p id="empty">Agrega un Actor</p>
                     :
-                    actors.map((actor) =>
+                    actores.map((actor) =>
                         <Actor key={actor.id} {...actor}/>
                     )
                 }
--- src/components/App.js
+++ src/components/App.js
@@ -7,61 +7,57 @@ import axios from 'axios';
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            shows: {},
+        this.state= {
+            shows: [],
             counter:0
         };
-        this.addSerie = this.addSerie.bind(this);
+        this.addShows = this.addShows.bind(this);
         this.removeShows = this.removeShows.bind(this);
     }
 
-    addSerie(id, name) {
+    addShows(id, name) {
 
         let actores = [];
-        let check;
 
         axios.get(`http://api.tvmaze.com/shows/94/cast`)
             .then(res => {
                 const personas = res.data;
-                {(personas.length === 0) ? check=false
-                    :
-                    check=true;
-                    personas.map((item) =>
-                        actores = [
-                            ...actores,
-                            { id: item.person.id, name: item.person.name, character: item.character.name, photo: item.character.image.original }
-                        ]
-                    )
-                }
-                let { shows }=this.state.shows;
 
-                if(check){
-                    shows[this.state.counter]={id:id, name:name, actores: actores};
-                    this.setState({ shows: shows, counter: this.state.counter+1 });
-                }else{
-                    this.setState({ shows: shows, msg: 'Serie agregada correctamente', counter: this.state.counter+1 });
-                }
+                personas.map((item) =>
+                    actores = [
+                        ...actores,
+                        {
+                            id: item.person.id,
+                            name: item.person.name,
+                            character: item.character.name,
+                            photo: item.character.image.original
+                        }
+                    ]
+                );
 
-        });
+                let shows = [...this.state.shows, {id: id, name: name, actores: actores}];
+                this.setState({shows: shows, msg: "Serie agregada exitosamente" ,counter: this.state.counter + 1});
+            });
     }
 
     removeShows(id){
-        const actors = this.state.actors.filter(x => x.id !== id);
-        this.setState({actors})
+        const shows = this.state.shows.filter(x => x.id !== id);
+        this.setState({shows: shows, msg: "Serie borrada exitosamente", counter: this.state.counter})
     }
 
     render() {
-        const { shows } = this.state.shows;
+        const shows  = this.state.shows;
         return (
             <div id="app" >
-                <AddActorForm onNewActor = { this.addSerie }/>
-                {(shows.length === 0) ? <p id="empty">Agrega un Actor</p>
+                <AddActorForm onNewActor = { this.addShows }/>
+                {(shows.length === 0) ? <p id="empty">Empieza agregando una serie</p>
                     :
-                    shows.map((show) =>
-                        <Actores key={show.id} name = {show.name} actors = {show.actores} remove={this.removeShows}/>
+                    shows.map((item) =>
+                        <Actores id={item.id} {...item} remove={this.removeShows}/>
                     )
                 }
-            </div> )
+            </div>
+        );
     }
 }
 
